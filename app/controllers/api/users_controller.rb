7015fc8f87fe5584745f08ec90756ee3fccf558a
bryewalks <bryewalks@gmail.com>
2019-07-25 17:38:19 -0500

    initial commit

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,21 @@
+class Api::UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: {message: "User created successfully"}, status: :created
+    else
+      render json: {errors: user.errors.full_messages}, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params
+      .require(:user)
+      .permit(:name, :email, :password, :password_confirmation)
+  end
+
+end
+
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  namespace :api do
+    resources :users, only: [:create]
+  end
+end
