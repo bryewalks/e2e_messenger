2dad43fc2726e41ae713584dd350b527dec4118d
bryewalks <bryewalks@gmail.com>
2019-08-03 21:57:07 -0500

    fixed missplaced before actions

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,5 +1,4 @@
 class Conversation < ApplicationRecord
-  # before_action :authenticate_user
   belongs_to :author, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
   has_many :messages, -> { order(created_at: :asc) }, dependent: :destroy
