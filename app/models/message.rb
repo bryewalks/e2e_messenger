c0848d55b288f6c0d5ef469baaa30e53e3a3bdc5
bryewalks <bryewalks@gmail.com>
2019-08-15 22:52:12 -0500

    added current user to view

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,8 @@ class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :user
   validates :body, presence: true
+
+  def current_user?(input_user)
+    self.user == input_user
+  end
 end
