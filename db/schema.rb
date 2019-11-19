bd748cab3eea441df03c0463c3816af2fbdd1e9a
brye <bryewalks@gmail.com>
2019-11-18 22:45:48 -0600

    removed email. using users name for auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_215029) do
+ActiveRecord::Schema.define(version: 2019_11_19_040939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2019_09_24_215029) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
     t.string "password_digest"
     t.boolean "admin", default: false
     t.datetime "created_at", null: false
